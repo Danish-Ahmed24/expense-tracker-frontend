32bb9fd1dda2237f628c7c635175c57d4466e847
Danish-Ahmed24 <danishsp21@gmail.com>
2025-07-16 22:51:14 +0500

    deploying

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,10 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:"/expense-tracker-frontend",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
+    },  
   },
 })
